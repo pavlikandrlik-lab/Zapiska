d838e60bdd1d19e8947e67a6895808fc85ceacb4
Ing. Pavel Andrlík <pavlik.andrlik@gmail.com>
2026-04-20 17:23:00 +0200

    feat(servicedesk): AddServiceDeskIntegration DI + fail-fast bez connection stringu

---
+++ PmTracker.ServiceDesk.Sql/ServiceDeskServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using PmTracker.ServiceDesk.Contracts;
+
+namespace PmTracker.ServiceDesk.Sql;
+
+public static class ServiceDeskServiceCollectionExtensions
+{
+    public static IServiceCollection AddServiceDeskIntegration(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddOptions<TicketingOptions>()
+            .Bind(configuration.GetSection(TicketingOptions.SectionName))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        var options = configuration.GetSection(TicketingOptions.SectionName).Get<TicketingOptions>()
+            ?? new TicketingOptions();
+
+        if (!options.Enabled)
+        {
+            services.AddScoped<ITicketingQueryService, DisabledTicketingQueryService>();
+            return services;
+        }
+
+        var connectionString = configuration.GetConnectionString(options.ConnectionStringName);
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException(
+                $"ServiceDesk integrace je zapnutá (Ticketing:Enabled=true), ale ConnectionString '{options.ConnectionStringName}' je prázdný. " +
+                $"Buď nastav connection string, nebo vypni Ticketing:Enabled.");
+        }
+
+        services.AddDbContext<TicketingReadOnlyDbContext>(opts =>
+            opts.UseSqlServer(connectionString, sql =>
+                sql.CommandTimeout(options.CommandTimeoutSeconds)));
+
+        services.AddScoped<SqlTicketingQueryService>();
+        services.AddScoped<ITicketingQueryService>(sp =>
+            new CachingTicketingQueryService(sp.GetRequiredService<SqlTicketingQueryService>()));
+
+        return services;
+    }
+}
